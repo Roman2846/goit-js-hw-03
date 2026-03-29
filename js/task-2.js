564fe396bf2f57ea6ff0f665cca859388c02ee39
Roman <krasivskyi01@gmail.com>
2026-03-29 18:08:03 +0300

    hm-js-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,10 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = [...firstArray, ...secondArray];
+  const newArray = firstArray.concat(secondArray);
 
   if (newArray.length > maxLength) {
     return newArray.slice(0, maxLength);
-  } else {
-    return newArray;
   }
+  return newArray;
 }
 
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
